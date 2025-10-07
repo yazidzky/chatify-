cc142b1daac1ee6ec5cbc22f32606fac273525d4
zinky <yazidzinky98@gmail.com>
2025-10-07 23:55:25 +0700

    #

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -7,7 +7,7 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongodb connected:", conn.connection.host);
   } catch (error) {
-    console.log("eror connection to mongoDB :", error);
+    console.log("error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
